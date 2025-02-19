c41141f21053e989fa22a90ba00a8f4453045ad2
minerva <minerva@Minervas-MacBook-Air.local>
2025-02-19 20:15:35 +0000

    nýjar breytingar

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,10 @@
-import pluginJs from '@eslint/js';
-import globals from 'globals';
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  {
-    ...pluginJs.configs.recommended,
-    rules: {
-      // Disallow console.log
-      'no-console': [
-        'error',
-        { allow: ['info', 'group', 'groupEnd', 'error'] },
-      ],
-    },
-  },
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginReact.configs.flat.recommended,
 ];
